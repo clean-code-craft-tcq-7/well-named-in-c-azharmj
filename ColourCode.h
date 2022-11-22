e808e1098993e9d63a9b8d50e0f0189c2084e6c0
azharmj <46325751+azharmj@users.noreply.github.com>
2022-11-22 14:19:51 +0530

    Create ColourCode.h

---
+++ ColourCode.h
@@ -0,0 +1,10 @@
+
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
